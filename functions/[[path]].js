265360c60ff90ec376a364db9fc1b45b60635147
Gabriel Abubakar <gabrielogrima@gmail.com>
2023-07-15 01:32:43 +0100

    functions edited out

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,12 +1,12 @@
-import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
+// import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
 
-// @ts-ignore
-import * as build from "../build";
+// // @ts-ignore
+// import * as build from "../build";
 
-const handleRequest = createPagesFunctionHandler({
-    build
-});
+// const handleRequest = createPagesFunctionHandler({
+//     build
+// });
 
-export function onRequest(context) {
-    return handleRequest(context);
-}
+// export function onRequest(context) {
+//     return handleRequest(context);
+// }
